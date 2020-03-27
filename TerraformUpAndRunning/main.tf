110448769f0fe71009bc82c56e6b4c82f4b91632
Kendrick Shaw <kms15@case.edu>
2020-03-27 16:26:04 +0000

    minimal AWS web server

--- TerraformUpAndRunning/main.tf
+++ TerraformUpAndRunning/main.tf
@@ -19,11 +19,31 @@ data "aws_ami" "ubuntu" {
     owners = ["099720109477"] # Canonical
 }
 
+# create a security group to allow incoming traffic on 8080
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 # create a t2.micro instance
 resource "aws_instance" "example" {
     # ami = "ami-0c55b159cbfafe1f0" # from Terraform Up & Running
     ami = data.aws_ami.ubuntu.id
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    # launch a simple web server on startup
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "<html><head><title>Hello</title></head><body><h1>Hello world!</h1></body></html>" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
 
     tags = {
         Name = "terraform-example"
